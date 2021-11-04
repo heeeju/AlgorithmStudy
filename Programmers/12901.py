bbcc2b90854f4bb92e71e0997a231854c5bfd710
heeeju <0xf1ed2@gmail.com>
2021-11-04 13:20:29 +0900

    프로그래머스 12901 연습문제

--- Programmers/12901.py
+++ Programmers/12901.py
@@ -1,9 +1,7 @@
 def solution(a, b):
-    day = 0
+    day = b
     days = [31,29,31,30,31,30,31,31,30,31,30,31]
     data = ['SUN','MON','TUE','WED','THU','FRI','SAT']
     for i in range(a-1):
         day += days[i]
-    day += b
-    answer = data[(4+day)%7]
-    return answer
+    return data[(4+day)%7]
