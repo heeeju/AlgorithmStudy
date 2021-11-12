7e2bf4729e63a24193af5ca125a11ff83c4e70a6
heeeju <0xf1ed2@gmail.com>
2021-11-12 14:03:23 +0900

    프로그래머스 12910 나누어 떨어지는 숫자 배열

---
+++ Programmers/12910.py
@@ -0,0 +1,5 @@
+def solution(arr, divisor):
+    answer = [x for x in arr if x % divisor == 0]
+    return sorted(answer) if len(answer) != 0 else [-1]
+
+    #return sorted( [x for x in arr if x % divisor == 0]) or [-1]
