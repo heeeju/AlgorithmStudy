e35a229321f1dc9b1ad6a41ddc6756caf17c5961
heeeju <0xf1ed2@gmail.com>
2021-11-22 22:50:46 +0900

    프로그래머스 42746 가장 큰 수

--- Programmers/42746.py
+++ Programmers/42746.py
@@ -1,6 +1,6 @@
 def solution(numbers):
     numbers = list(map(str, numbers))
-    numbers = sorted(numbers, key = lambda x : (x*4)[:4], reverse = True)
+    numbers = sorted(numbers, key = lambda x : x*4, reverse = True) #x*3도 가능!
     return str(int(''.join(numbers)))
     #return ''.join(numbers) => X
     # 반례 [0,0,0,0]
