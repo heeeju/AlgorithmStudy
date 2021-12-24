a581c8952836b6cdbee4d1dcb3c49e7f2acc4a77
heeeju <0xf1ed2@gmail.com>
2021-12-24 10:29:45 +0900

    좋은 구간

---
+++ BOJ/1059.py
@@ -0,0 +1,16 @@
+L = int(input())
+S = sorted([0] + list(map(int, input().split())))
+N = int(input())
+
+if N in S:
+  print(0)
+else:
+  for i in range(len(S)-1):
+    left = S[i]
+    right = S[i+1]
+    if N in range(left, right):
+      break
+
+  left += 1
+  right -= 1
+  print((right - left) + (N-left)*(right-N))
