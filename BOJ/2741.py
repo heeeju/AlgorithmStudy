0804e9bb8ae528fdd1601455957e5232093518b4
heeeju <0xf1ed2@gmail.com>
2022-01-09 22:33:10 +0900

    BOJ 7568 덩치

--- BOJ/2741.py
+++ BOJ/2741.py
@@ -1,3 +1,12 @@
 n = int(input())
-for i in range(1,n+1):
-  print(i)
+info = []
+for _ in range(n):
+  w, h = map(int, input().split())
+  info.append([w, h])
+
+for i in range(n):
+  cnt = 1
+  for j in range(n):
+    if j != i and info[i][0] < info[j][0] and info[i][1] < info[j][1]:
+      cnt += 1
+  print(cnt, end = ' ')
