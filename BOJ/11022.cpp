ddf353082c56f3b0db24ba981b23827fb7b2fd60
heeeju <0xf1ed2@gmail.com>
2022-01-12 12:33:24 +0900

    BOJ 11022 A+B - 8

---
+++ BOJ/11022.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+
+  int n, a, b;
+  cin >> n;
+  for(int i=0; i<n; i++){
+    cin >> a;
+    cin >> b;
+    cout << "Case #" << i+1 << ": "<< a << " + " << b << " = " << a+b << "\n";
+  }
+
+  return 0;
+}
