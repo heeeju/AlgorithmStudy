b6730111e4b816a07d5a659e21f1a85de89ce068
희주 <heeju@huijuui-MacBookPro.local>
2022-03-09 21:20:41 +0900

    BOJ 1934 최소공배수

---
+++ BOJ/1934.py
@@ -0,0 +1,11 @@
+def gcd(x,y):
+  while y:
+    x, y = y, x % y
+  return x
+def lcm(x,y):
+  return x * y // gcd(x,y)
+
+T = int(input())
+for _ in range(T):
+  a, b = map(int, input().split())
+  print(lcm(a,b))
